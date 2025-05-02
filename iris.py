07d2695ed81b01e77bb89205940d81a5437c9c99
Orla Woods <orla.woods@gmail.com>
2025-05-02 23:12:57 +0100

    adding iris csv file

--- iris.py
+++ iris.py
@@ -2,23 +2,11 @@
 # This script loads the iris dataset
 # Author: Orla Woods
 
-# Using the import in Python option from: https://archive.ics.uci.edu/dataset/53/iris
-# Install: pip install ucimlrepo in the cmd module (done)
+# Using the "download" option from https://archive.ics.uci.edu/dataset/53/iris
 
-# from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-iris = fetch_ucirepo(id=53) 
-  
-# data (as pandas dataframes) 
-X = iris.data.features 
-y = iris.data.targets 
-  
-# metadata 
-print(iris.metadata) 
-  
-# variable information 
-print(iris.variables) 
+
+
+
 
 
 
